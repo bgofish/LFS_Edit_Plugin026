c8fe0d501dbbcb0910f67e08a8daf7f72c14bfcd
bb6 <73919236+bgofish@users.noreply.github.com>
2026-05-07 15:38:22 +1200

    0.2.7

--- __init__.py
+++ __init__.py
@@ -3,9 +3,11 @@
 
 import lichtfeld as lf
 from .panels.transform_panel import TransformPanel
+from .panels.point_align_panel import PointAlignPanel
 from .operators.align_picker import ALIGN_OT_pick_point
+from .operators.point_align_picker import POINTALIGN_OT_pick_point
 
-_classes = [TransformPanel, ALIGN_OT_pick_point]
+_classes = [TransformPanel, PointAlignPanel, ALIGN_OT_pick_point, POINTALIGN_OT_pick_point]
 
 
 def on_load():
@@ -16,7 +18,9 @@ def on_load():
 
 def on_unload():
     from .panels.transform_panel import _remove_align_draw_handler
+    from .panels.point_align_panel import remove_point_align_draw_handler
     _remove_align_draw_handler()
+    remove_point_align_draw_handler()
     for cls in reversed(_classes):
         lf.unregister_class(cls)
     lf.log.info("EDIT unloaded")
